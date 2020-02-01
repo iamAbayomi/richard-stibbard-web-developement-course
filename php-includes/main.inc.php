548c3ccdba3ebd6859560311a79601c8833b357d
Iamabayomi <Oladinifolawiyo@gmail.com>
2020-02-01 04:34:54 +0100

    Removed the trash can when there are no favourites

--- php-includes/main.inc.php
+++ php-includes/main.inc.php
@@ -3,6 +3,14 @@ $favsList = showMovies('favs');
 $nonfavsList = showMovies('non_favs');
 $testMovies = testMovies();
 
+if ($favsList==""){
+	$favsTitle = "You have no favourites";
+	$trashClass = "hidden";
+} else {
+	$favsTitle = "Favourites";
+	$trashClass = "";
+}
+
 switch($testMovies) {
     
     case "invalid_id":
@@ -11,6 +19,14 @@ switch($testMovies) {
         echo "</div>";
         include 'footer.inc.php';
         exit;
+		
+	case "no_data":
+		echo "<div class='message alert'>";
+		echo "<h2>No movies in database: Add movies below</h2>";
+		echo "</div>";
+		include 'admin-movies.inc.php';
+		include 'footer.inc.php';
+		exit;
 		
     case "no_id":
         $greeting = showUsers('get_name');
@@ -22,36 +38,31 @@ switch($testMovies) {
 
 }
 
-if ($testMovies == "no_data") {
-	echo "<div class='message alert'>";
-    echo "<h2>No movies in database: Add movies below</h2>";
-    echo "</div>";
-    include 'admin-movies.inc.php';
-	include 'footer.inc.php';
-    exit;	
-} else {
-	echo "<nav class='favs_list'>";
-    echo "<h2>Favourites</h2>";
-    echo "<ul class='favs'>";
-	echo $favsList;
-    echo "</ul>";
-	echo "<div class='trash'></div>";
-    echo "</nav>";
-}
- 
-if (!isset($movieID)) { // show movie list
-    echo "<section class='movie_list'>";
-    echo $greeting;
-    echo "<p class='welcome'>Here are some movies you might like.";
-    echo "Click on the heart icon to add them to your favourites list.</p>";
-    echo "<ul>";
-    echo $nonfavsList;
-    echo "</ul>";
-    echo "</section>";
-} else { //show single movie
-    echo "<section class='movie_single'>";
-    echo $singleMovie;
-    echo "</section>";
+echo "<nav class='favs_list'>";
+echo "<h2>$favsTitle</h2>";
+echo "<ul class='favs'>";
+echo $favsList;
+echo "</ul>";
+echo "<div class='trash $trashClass'></div>";
+echo "</nav>";
+
+switch($testMovies) {
+	case "no_id":
+		echo "<section class='movie_list'>";
+		echo $greeting;
+		echo "<p class='welcome'>Here are some movies you might like.";
+		echo "Click on the heart icon to add them to your favourites list.</p>";
+		echo "<ul>";
+		echo $nonfavsList;
+		echo "</ul>";
+		echo "</section>";
+		break;
+	
+	case "id_set":
+		echo "<section class='movie_single'>";
+		echo $singleMovie;
+		echo "</section>";
+		break;
 }
 
 ?>
